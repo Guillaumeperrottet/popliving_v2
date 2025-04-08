25d1ae13c5442ce1cca426ab86ebc641c5599ffb
guillaume perrottet <perrottet.guillaume.97@gmail.com>
2025-04-08 11:45:52 +0200

    page situation

---
+++ app/situation/page.jsx
@@ -0,0 +1,42 @@
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+
+export default function Situation() {
+  return (
+    <>
+      <Navbar />
+      <main className="mt-20">
+        {/* Section principale */}
+        <section
+          className="relative h-[60vh] bg-cover bg-center flex flex-col items-center justify-center text-center"
+          style={{
+            backgroundImage: "url('/images/situation-background.jpg')",
+          }}
+        >          <div className="relative z-10">
+            <p className="text-white text-lg mt-4">
+              Situé à Riaz, au cœur de la Gruyère, cet immeuble bénéficie d'une localisation idéale. Vous trouverez à proximité des commerces et des transports publics facilitant vos déplacements dans toute la région.
+            </p>
+          </div>
+        </section>
+
+
+        {/* Section carte */}
+        <section className="container mx-auto px-8 py-16">
+          <h2 className="text-3xl font-bold mb-8">Localisation</h2>
+          <div className="w-full h-96">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5478.674819123118!2d7.0617802!3d46.6398508!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x478e630072868315%3A0x404aa784df5c03b!2sPopliving!5e0!3m2!1sfr!2sch!4v1744105391785!5m2!1sfr!2sch"
+              width="100%"
+              height="100%"
+              style={{ border: 0 }}
+              allowFullScreen=""
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
+        </section>
+      </main>
+      <Footer />
+    </>
+  );
+}
